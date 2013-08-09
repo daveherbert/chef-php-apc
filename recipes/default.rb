3f476f75adb0000b7cab9ac53db070b51a3f6263
Sebastian Johnsson <sebastian@agiley.se>
2013-08-09 14:56:48 +0200

    the php_pear package is case-sensitive - it needs to be APC and not apc

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@ include_recipe "php"
 package "libpcre3-dev"
 
 # Install APC.
-php_pear "apc" do
+php_pear "APC" do
   version node['php']['apc']['version']
   directives(node['php']['apc']['configuration'])
   action :install
