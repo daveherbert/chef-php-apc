956c0716ecd991b01a163290ebbbb133601ed6bc
Sebastian Johnsson <sebastian@agiley.se>
2013-08-09 14:25:13 +0200

    Update default attribs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,16 @@
 default['php']['apc']['version']          =   '3.1.9'
 default['php']['apc']['configuration']    =   {
-  'shm_size'          =>    128,
-  'shm_segments'      =>    1,
-  'ttl'               =>    7200,
-  'user_ttl'          =>    7200,
-  'enable_cli'        =>    1,
-  'cache_by_default'  =>    1,
-  'max_file_size'     =>    10,
-  'stat'              =>    1
+  'enabled'               =>    1,
+  'shm_segments'          =>    1,
+  'shm_size'              =>    '128M',
+  'ttl'                   =>    7200,
+  'user_ttl'              =>    7200,
+  'gc_ttl'                =>    3600,
+  'cache_by_default'      =>    1,
+  'include_once_override' =>    0,
+  'max_file_size'         =>    '2M',
+  'stat'                  =>    1,
+  'write_lock'            =>    1,
+  'slam_defense'          =>    0,
+  'enable_cli'            =>    0
 }
