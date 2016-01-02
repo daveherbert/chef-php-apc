bce4da1bcd2c2089dbd82bb1e6dd26f2d53fe37b
Dave Herbert <dave@daveherbert.co.uk>
2016-01-02 19:41:26 +0000

    Add cookbook name to metadata for use with berkshelf

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "Sebastian Johnsson"
 maintainer_email "sebastian@agiley.se"
 license          "MIT"
+name             "php-apc"
 description      "Installs and configures PHP-APC."
 version          "0.1"
 
