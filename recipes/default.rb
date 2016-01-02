9074ec0fee30c7ecacd22791dec9356bf4792aea
Dave Herbert <dave@daveherbert.co.uk>
2016-01-02 20:14:29 +0000

    Check platform and install correct packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,22 @@
 include_recipe "php"
 
-# Required to install APC.
-package "libpcre3-dev"
-
-# Install APC.
-php_pear "apc" do
-  version node['php']['apc']['version']
-  directives(node['php']['apc']['configuration'])
-  action :install
+case node['platform_family']
+when 'rhel', 'fedora'
+  %w{ httpd-devel pcre pcre-devel }.each do |pkg|
+    package pkg do
+      action :install
+    end
+  end
+  php_pear "apc" do
+    version node['php']['apc']['version']
+    directives(node['php']['apc']['configuration'])
+    action :install
+  end
+when 'debian'
+  package 'libpcre3-dev' do
+    action :install
+  end
+  package 'php-apc' do
+    action :install
+  end
 end
